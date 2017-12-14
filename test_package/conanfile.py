9c1371330c941889604903774cfc265c97ec0e15
Jerry W <jerrywiltse@gmail.com>
2017-12-14 14:34:34 -0500

    apply standard test_package conanfile

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -4,25 +4,15 @@
 from conans import ConanFile, CMake, tools, RunEnvironment
 import os
 
-default_user = "wi3ard"
-default_channel = "testing"
-
-# This easily allows to copy the package in other user or channel
-channel = os.getenv("CONAN_CHANNEL", default_channel)
-username = os.getenv("CONAN_USERNAME", default_user)
 
 class TestPackageConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    requires = "mysql-connector-c/6.1.11@%s/%s" % (username, channel)
     generators = "cmake"
 
     def build(self):
         cmake = CMake(self)
         cmake.configure()
         cmake.build()
-
-    def imports(self):
-        self.copy("*.dll", dst="bin", src="bin")
 
     def test(self):
         with tools.environment_append(RunEnvironment(self).vars):
