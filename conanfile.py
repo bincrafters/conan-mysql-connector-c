ab885b7958c9b18e233e7d40c79dffc93dbdd568
Alex Kucher <a.kucher@avantize.com>
2018-01-19 14:44:30 +0200

    Update to the latest template

--- conanfile.py
+++ conanfile.py
@@ -4,6 +4,7 @@
 from conans import ConanFile, CMake, tools
 import os, shutil
 
+
 class MysqlConnectorCConan(ConanFile):
     name = "mysql-connector-c"
     version = "6.1.11"
@@ -16,35 +17,31 @@ class MysqlConnectorCConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
     options = {"shared": [True, False]}
     default_options = "shared=False"
+    source_subfolder = "source_subfolder"
+    build_subfolder = "build_subfolder"
 
     def source(self):
         source_url = "http://dev.mysql.com/get/Downloads/Connector-C"
-        archive_name = self.name + "-" + self.version + "-src"
-        ext = "tar.gz"
-        tools.get("{0}/{1}.{2}".format(source_url, archive_name, ext))
-        os.rename(archive_name, "sources")
-        
-        sources_cmake = os.path.join("sources", "CMakeLists.txt")
-        sources_cmake_orig = os.path.join("sources", "CMakeListsOriginal.txt")
-        
+        extracted_dir = self.name + "-" + self.version + "-src"
+        tools.get("{0}/{1}.tar.gz".format(source_url, extracted_dir))
+        os.rename(extracted_dir, self.source_subfolder)
+
+        sources_cmake = os.path.join(self.source_subfolder, "CMakeLists.txt")
+        sources_cmake_orig = os.path.join(self.source_subfolder, "CMakeListsOriginal.txt")
         os.rename(sources_cmake, sources_cmake_orig)
         os.rename("CMakeLists.txt", sources_cmake)
 
     def build(self):
         cmake = CMake(self)
 
-        if self.options.shared:
-            cmake.definitions["DISABLE_SHARED"] = "OFF"
-            cmake.definitions["DISABLE_STATIC"] = "ON"
-        else:
-            cmake.definitions["DISABLE_SHARED"] = "ON"
-            cmake.definitions["DISABLE_STATIC"] = "OFF"
+        cmake.definitions["DISABLE_SHARED"] = not self.options.shared
+        cmake.definitions["DISABLE_STATIC"] = self.options.shared
 
         if self.settings.compiler == "Visual Studio":
             if self.settings.compiler.runtime == "MD" or self.settings.compiler.runtime == "MDd":
-                cmake.definitions["WINDOWS_RUNTIME_MD"] = "ON"
+                cmake.definitions["WINDOWS_RUNTIME_MD"] = True
 
-        cmake.configure(source_dir="sources")
+        cmake.configure(source_folder=self.source_subfolder,build_folder=self.build_subfolder)
         cmake.build()
         cmake.install()
 
